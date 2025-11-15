3209452b07c9728784fd7bea9e35a7f74c3c9927
Yves <yalikalfic@yahoo.com>
2025-11-15 14:21:41 -0500

    Initial Commit

---
+++ budget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  budget
+//
+//  Created by Yves Alikalfic on 2025-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ budget/budgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetApp.swift
+//  budget
+//
+//  Created by Yves Alikalfic on 2025-11-15.
+//
+
+import SwiftUI
+
+@main
+struct budgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
